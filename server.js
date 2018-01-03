fdd11fb27db5c93ea123b85f2f9d208f5c26e758
Brennan Glynn <brennanglynn@u.boisestate.edu>
2018-01-03 12:58:29 -0700

    .

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.get("/api/hello", (req, res) => {
 });
 
 // Serve static assets
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
+app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
 app.listen(port, () => console.log(`Listening on ${port}`));
